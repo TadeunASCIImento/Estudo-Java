0f4dc047aac5c96449bcad3d56c12a522b79ad90
TadeunASCIImento <tadeunascimento36@gmail.com>
2019-06-27 10:27:20 -0300

    switch-case controlado por string

--- instrucoes-de-controle/SwitchCase.java
+++ instrucoes-de-controle/SwitchCase.java
@@ -7,29 +7,35 @@ public class SwitchCase {
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
+		boolean continueLoop = true;
 
-		while (true) {// cria um laço while infinito
+		while (continueLoop) {// cria um laço while infinito
 
 			System.out.println("\n\t\tDigite uma opção?\n");
-			System.out.println("\t\t[ cancelar ]\t\t[  Salvar  ]\t\t[ Deletar  ]");
+			System.out.println("\t\t[ Cancelar ]\t\t[  Salvar  ]\t\t[ Deletar  ]\t\t[ Sair  ]");
 			String strCommand = input.next();
 
 			switch (strCommand) {// controlado por String
 
 			case "Cancelar":
-				System.out.println("\t\tCancelando.....");
+				System.out.println("\t\tCancelando.....\n");
 				break;
 
 			case "Salvar":
-				System.out.println("\t\tSalvando.......");
+				System.out.println("\t\tSalvando.......\n");
 				break;
 
 			case "Deletar":
-				System.out.println("\t\tDeletando......");
+				System.out.println("\t\tDeletando......\n");
+				break;
+
+			case "Sair":
+				continueLoop = false;
+				System.out.println("\t\tTerminado......\n");
 				break;
 
 			default:
-				System.out.println("\t\tInválido.......");
+				System.out.println("\t\tInválido.......\n");
 				break;
 
 			}
