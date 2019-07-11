618704d7e8cbbfe02e7a3d3d2e5b108bfa340a8e
TadeunASCIImento <tadeunascimento36@gmail.com>
2019-07-11 09:12:33 -0300

    Laço while

---
+++ instrucoes-de-controle/While.java
@@ -0,0 +1,21 @@
+package br.com.estudo;
+
+public class While {
+
+	public static void main(String[] args) {
+
+		boolean continueLoop = true;
+		int contador = 0;
+
+		while (continueLoop) {
+			if (contador < 20)
+				System.out.println("Passo: " + contador);
+			else
+				System.exit(1);
+			contador++;
+
+		}
+
+	}
+
+}
