0cb4ee8212da8d4ec79a50d72425c521febeb658
TadeunASCIImento <tadeunascimento36@gmail.com>
2019-07-15 16:36:41 -0300

    Usando uma enum para declarar constantes de um menu de opções.

---
+++ Enums/MenuOpcoesUsandoEnum.java
@@ -0,0 +1,54 @@
+package br.com.estudo;
+
+// declarando a enum com as constantes do menu de opçoes
+enum Opcoes {
+
+	INSERIR(1), ALTERAR(2), DELETAR(3), VISUALIZAR(4);
+
+	int intValor;
+
+	Opcoes(int valor) {
+		this.intValor = valor;
+	}
+
+	int getValor() {
+		return intValor;
+	}
+}
+
+// classe aninhada para teste do menu
+public class MenuOpcoesUsandoEnum {
+
+	public static void main(String[] args) {
+		// chamada ao metodo selecionarOpcao(),
+		// passando as constantes da enum como parâmetro
+		selecionarOpcao(Opcoes.VISUALIZAR);
+		selecionarOpcao(Opcoes.ALTERAR);
+		selecionarOpcao(Opcoes.DELETAR);
+		selecionarOpcao(Opcoes.INSERIR);
+
+	}
+
+	// metodo que cria o menu de opções
+	private static void selecionarOpcao(Opcoes opcao) {
+
+		switch (opcao) {
+
+		case VISUALIZAR:
+			System.out.println("\nMostrando");
+			break;
+		case INSERIR:
+			System.out.println("\nInserindo");
+			break;
+		case ALTERAR:
+			System.out.println("\nAlterando");
+			break;
+		case DELETAR:
+			System.out.println("\nDeletando");
+			break;
+
+		}
+
+	}
+
+}
