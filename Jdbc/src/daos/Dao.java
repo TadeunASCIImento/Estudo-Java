02b26956ce097da698c6c7446d1d4a5d05b2f763
Tadeu Nascimento <47820260+TadeunASCIImento@users.noreply.github.com>
2020-04-04 17:00:30 -0300

    Classe de conexão com o banco MySQL.

---
+++ Jdbc/src/daos/Dao.java
@@ -0,0 +1,39 @@
+package br.com.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public abstract class Dao {
+
+	private final String SERVIDOR = "localhost:3306";
+	private final String TIME_ZONE = "?serverTimezone=UTC";
+	private final String NOME_BANCO = "nomeBanco";
+	private final String USUARIO_BANCO = "usuarioBanco";
+	private final String SENHA_BANCO = "senhaBanco";
+
+  protected PreparedStatement pstm;
+	protected ResultSet resultado;
+	protected Connection conn;
+
+	public Connection obterConexao() {
+
+		String url_banco = "jdbc:mysql://" + SERVIDOR + "/" + NOME_BANCO + TIME_ZONE;
+
+    try {
+
+    	conn = DriverManager.getConnection(url_banco, USUARIO_BANCO, SENHA_BANCO);
+
+    } catch (SQLException e) {
+
+    	e.printStackTrace();
+
+    }
+
+    return conn;
+
+	}
+
+}
