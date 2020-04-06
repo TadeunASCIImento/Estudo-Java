f4aa73b8d7a549887ee57b7e170b64a235e84f7d
Tadeu do Nascimento <tadeunascimento36@gmail.com>
2020-04-06 11:43:42 -0300

    Classe DaoUsuario e método para validar usuário no banco.

---
+++ Jdbc/src/daos/DaoUsuario.java
@@ -0,0 +1,33 @@
+package br.com.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import br.com.entities.Entitie;
+import br.com.entities.Usuario;
+
+public class DaoUsuario extends Dao implements DaoFunctions, InstrucoesSql {
+
+	public boolean validarUsuario(String user, String pass) {
+
+		try {
+      this.obterConexao();
+			this.pstm = this.conn.prepareStatement(VALIDAR_USUARIO);
+			this.resultado = this.pstm.executeQuery();
+			  while (this.resultado.next()) {
+				Usuario usuario = new Usuario();
+				usuario.setUsuario(this.resultado.getString("usuario"));
+				usuario.setSenha(this.resultado.getString("senha"));
+				if (usuario.getUsuario().equalsIgnoreCase(user) && usuario.getSenha().equalsIgnoreCase(pass))
+					return true;
+			}
+
+		} catch (SQLException exc) {
+			System.out.println("Erro ao conectar com banco.");
+			exc.printStackTrace();
+
+		}
+		return false;
+
+	}
+}
