6157dd7ddefc7b94bf3e195ec9941a6369cb17c1
Tadeu Nascimento <47820260+TadeunASCIImento@users.noreply.github.com>
2020-04-07 17:57:11 -0300

    Implementado método atualizar usuário.

--- Jdbc/src/daos/DaoUsuario.java
+++ Jdbc/src/daos/DaoUsuario.java
@@ -9,25 +9,23 @@ import br.com.entities.Usuario;
 public class DaoUsuario extends Dao implements DaoFunctions, InstrucoesSql {
 
 	public boolean validarUsuario(String user, String pass) {
-
 		try {
-				this.obterConexao();
-				this.pstm = this.conn.prepareStatement(VALIDAR_USUARIO);
-				this.resultado = this.pstm.executeQuery();
+			this.obterConexao();
+			this.pstm = this.conn.prepareStatement(VALIDAR_USUARIO);
+			this.resultado = this.pstm.executeQuery();
 			while (this.resultado.next()) {
-				Usuario usuario = new Usuario();
-				usuario.setUsuario(this.resultado.getString("usuario"));
-				usuario.setSenha(this.resultado.getString("senha"));
-				if (usuario.getUsuario().equals(user) && usuario.getSenha().equals(pass))
-				return true;
+			Usuario usuario = new Usuario();
+			usuario.setUsuario(this.resultado.getString("usuario"));
+			usuario.setSenha(this.resultado.getString("senha"));
+			if (usuario.getUsuario().equals(user) && usuario.getSenha().equals(pass))
+			return true;
 			}
 
 		} catch (SQLException exc) {
-				System.out.println("Erro ao conectar ao banco.");
-				exc.printStackTrace();			
+			System.out.println("Erro ao conectar ao banco.");
+			exc.printStackTrace();
 		}
 		return false;
-
 	}
 
 
@@ -51,29 +49,26 @@ public class DaoUsuario extends Dao implements DaoFunctions, InstrucoesSql {
 
 	@Override
 	public Usuario findOne(int id) {
-
 		Usuario usuario = new Usuario();
 		try {
-				this.obterConexao();
-				this.pstm = this.conn.prepareStatement(SELECT_USUARIO + id);
-				this.resultado = this.pstm.executeQuery();
+			this.obterConexao();
+			this.pstm = this.conn.prepareStatement(SELECT_USUARIO + id);
+			this.resultado = this.pstm.executeQuery();
 			while (this.resultado.next()) {
-				usuario.setId(this.resultado.getInt("id"));
-				usuario.setNome(this.resultado.getString("nome"));
-				usuario.setCpf(this.resultado.getString("cpf"));
-				usuario.setUsuario(this.resultado.getString("usuario"));
-				usuario.setSenha(this.resultado.getString("senha"));
+			usuario.setId(this.resultado.getInt("id"));
+			usuario.setNome(this.resultado.getString("nome"));
+			usuario.setCpf(this.resultado.getString("cpf"));
+			usuario.setUsuario(this.resultado.getString("usuario"));
+			usuario.setSenha(this.resultado.getString("senha"));
 			}
 		} catch (SQLException exc) {
 			exc.printStackTrace();
-
 		}
 		return usuario;
 	}
 
 	@Override
 	public boolean delete(int id) {
-
 		try {
 			this.obterConexao();
 			this.pstm = this.conn.prepareStatement(DELETE_USUARIO + id);
@@ -82,25 +77,23 @@ public class DaoUsuario extends Dao implements DaoFunctions, InstrucoesSql {
 			exc.printStackTrace();
 		}
 		return true;
-
 	}
 
 	@Override
 	public List<Entitie> findAll() {
 		List<Entitie> listaUsuarios = new ArrayList<>();
-
 		try {
-				this.obterConexao();
-				this.pstm = this.conn.prepareStatement(LISTAR_USUARIOS);
-				this.resultado = this.pstm.executeQuery();
+			this.obterConexao();
+			this.pstm = this.conn.prepareStatement(LISTAR_USUARIOS);
+			this.resultado = this.pstm.executeQuery();
 			while (this.resultado.next()) {
-				Usuario usuario = new Usuario();
-				usuario.setId(this.resultado.getInt("id"));
-				usuario.setNome(this.resultado.getString("nome"));
-				usuario.setCpf(this.resultado.getString("cpf"));
-				usuario.setUsuario(this.resultado.getString("usuario"));
-				usuario.setSenha(this.resultado.getString("senha"));
-				listaUsuarios.add(usuario);
+			Usuario usuario = new Usuario();
+			usuario.setId(this.resultado.getInt("id"));
+			usuario.setNome(this.resultado.getString("nome"));
+			usuario.setCpf(this.resultado.getString("cpf"));
+			usuario.setUsuario(this.resultado.getString("usuario"));
+			usuario.setSenha(this.resultado.getString("senha"));
+			listaUsuarios.add(usuario);
 			}
 			return listaUsuarios;
 		} catch (SQLException exc) {
@@ -108,6 +101,26 @@ public class DaoUsuario extends Dao implements DaoFunctions, InstrucoesSql {
 			return null;
 		}
 	}
+
+	@Override
+	public boolean update(Entitie entitie) {
+		Usuario usuario = (Usuario) entitie;
+		try {
+			this.obterConexao();
+			this.pstm = this.conn.prepareStatement(UPDATE_USUARIO + usuario.getId());
+			pstm.setString(1, usuario.getNome());
+			pstm.setString(2, usuario.getCpf());
+			pstm.setString(3, usuario.getUsuario());
+			pstm.setString(4, usuario.getSenha());
+			pstm.executeUpdate();
+			return true;
+		} catch (Exception exc) {
+			return false;
+
+		}
+
+	}
+
 
 
 }
