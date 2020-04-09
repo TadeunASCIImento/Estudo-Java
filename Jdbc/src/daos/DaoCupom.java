df0dad75a09c7c75a3c42bbbb43d3cd38843bd48
Tadeu Nascimento <47820260+TadeunASCIImento@users.noreply.github.com>
2020-04-09 15:59:57 -0300

    Classe DaoCupom e o método para inserir um novo registro no banco.

---
+++ Jdbc/src/daos/DaoCupom.java
@@ -0,0 +1,31 @@
+package br.com.daos;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.entities.Cupom;
+import br.com.entities.Entitie;
+
+public class DaoCupom extends Dao implements DaoFunctions, InstrucoesSql {
+
+	@Override
+	public boolean add(Entitie entitie) {
+		Cupom cupom = (Cupom) entitie;
+		try {
+			this.obterConexao();
+			this.pstm = this.conn.prepareStatement(INSERT_CUPOM);
+			this.pstm.setLong(1, cupom.getCodCupom());
+			this.pstm.setString(2, cupom.getDescricao());
+			this.pstm.setLong(3, cupom.getNumPontos());
+			this.pstm.execute();
+			return true;
+		} catch (SQLException sqlExc) {
+			sqlExc.printStackTrace();
+			return false;
+		}
+	}
+
+
+
+}
