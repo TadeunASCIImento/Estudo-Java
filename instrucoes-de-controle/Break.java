e2bf115e3e9ad8602e68c21c046ba507be00b06e
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-09-25 22:59:18 -0300

    Melhorando o exemplo de uso da instrução break.

--- instrucoes-de-controle/Break.java
+++ instrucoes-de-controle/Break.java
@@ -1,22 +1,44 @@
 package br.com.estudo;
 
+import java.util.Scanner;
+
 public class Break {
+
+	private static Scanner scanner = new Scanner(System.in);
+
+	private static int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	static String inputELement = "Elemento para busca no array? ";
+	static String foundMessage = "Elemento localizado no array ";
+	static String notfoundMessage = "Elemento não localizado no array ";
 
 	public static void main(String[] args) {
 
-		System.out.print("\nNúmeros antes da interrupção: ");
-
-		// laço for controlado por contador
-		for (int cont = 1; cont <= 10; cont++) {
-
-			// verifica se cont é igual 5
-			if (cont == 7)
-
-				// interrompe o laço se cont for 7
-				break;// usando a instrução break;
-
-			System.out.printf("%d ", cont);// mostra cont diferente de 7
+		for (;;) {
+			showMessage(inputELement);
+			int elemento = input();
+			if (findOne(elemento, array))
+				break;
+			showMessage(notfoundMessage);
 		}
+		showMessage(foundMessage);
 
+	}
+
+	private static void showMessage(String message) {
+		System.out.println(message);
+	}
+
+	private static int input() {
+		return scanner.nextInt();
+	}
+
+	private static boolean findOne(int elemento, int array[]) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == elemento) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
