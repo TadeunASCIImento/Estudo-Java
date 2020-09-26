1943ad317ee29c30d249ae1e88d056ba5d8cfce0
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-09-26 01:50:56 -0300

    Melhorando exemplo de uso do foreach.

--- instrucoes-de-controle/ForEach.java
+++ instrucoes-de-controle/ForEach.java
@@ -1,38 +1,70 @@
 package br.com.estudo;
 
+import java.security.SecureRandom;
 import java.util.Scanner;
 
-public class Foreach {
+public class ForEach {
+
+	private static Scanner scanner = new Scanner(System.in);
+
+	static String sizeMessage = "Tamanho para o array?";
+	static String seedMessage = "Semente para o  array?";
+	static String elementNotFound = "elemento não encontrado";
+	static String elementFound = "elemento encontrado.";
+	static String elementsRandonArray = "elementos em randomArray: ";
 
 	public static void main(String[] args) {
+		showMessage(sizeMessage);
+		int size = getScannerInputInt();
+		showMessage(seedMessage);
+		int seed = getScannerInputInt();
+		int[] randomArray = getRandomArrayInt(size, seed);
+		showRandomArray(randomArray);
+		int element = getScannerInputInt();
+		boolean result = seekElement(element, randomArray);
+		showSearchResult(result);
 
-		Scanner scanner = new Scanner(System.in);
-
-		// cria um array de numeros
-		int numeros[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-		int valor;
-		boolean valorBusca = false;
-
-		System.out.printf("\n\tDigite um número: ");
-		valor = scanner.nextInt();
-
-		// Laço foreach
-		for (int numero : numeros) {
-			if (numero == valor) {
-				valorBusca = true;
-				break;
-			}
-		}
-
-		// Se o número for localizado no array exibe a mensagem.
-		if (valorBusca)
-			System.out.println("\nValor encontrado.");
-		else
-			// Senão exibe a mensagem
-			System.out.println("\nValor não encontrado.");
-
-		scanner.close();
 	}
 
+	private static void showRandomArray(int[] randomArray) {
+		showMessage(elementsRandonArray);
+		for (int e : randomArray) {
+			System.out.printf(" %d ", e);
+		}
+
+	}
+
+	private static void showMessage(String message) {
+		System.out.print(message);
+	}
+
+	private static int[] getRandomArrayInt(int size, int seed) {
+		SecureRandom secureRandom = new SecureRandom();
+		int[] randomArray = new int[size];
+		for (int i = 0; i < randomArray.length; i++) {
+			randomArray[i] = (1 + secureRandom.nextInt(seed));
+		}
+		return randomArray;
+	}
+
+	private static int getScannerInputInt() {
+		int valorInt = scanner.nextInt();
+		return valorInt;
+	}
+
+	private static boolean seekElement(int element, int[] randomArray) {
+		for (int e : randomArray) {
+			if (element == e) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static void showSearchResult(boolean result) {
+		if (result)
+			showMessage(elementFound);
+		else
+			showMessage(elementNotFound);
+	}
 }
