bf9499a1ac49d67a6e3464e620dde494af47ac6d
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-09-27 00:32:57 -0300

    Melhorando o exemplo de uso do switch-case.

--- instrucoes-de-controle/SwitchCase.java
+++ instrucoes-de-controle/SwitchCase.java
@@ -4,43 +4,59 @@ import java.util.Scanner;
 
 public class SwitchCase {
 
+	private static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
-
-		Scanner input = new Scanner(System.in);
-		boolean continueLoop = true;
-
-		while (continueLoop) {// cria um laço while infinito
-
-			System.out.println("\n\t\tDigite uma opção?\n");
-			System.out.println("\t\t[ Cancelar ]\t\t[  Salvar  ]\t\t[ Deletar  ]\t\t[ Sair  ]");
-			String strCommand = input.next();
-
-			switch (strCommand) {// controlado por String
-
-			case "Cancelar":
-				System.out.println("\t\tCancelando.....\n");
-				break;
-
-			case "Salvar":
-				System.out.println("\t\tSalvando.......\n");
-				break;
-
-			case "Deletar":
-				System.out.println("\t\tDeletando......\n");
-				break;
-
-			case "Sair":
-				continueLoop = false;
-				System.out.println("\t\tTerminado......\n");
-				break;
-
-			default:
-				System.out.println("\t\tInválido.......\n");
-				break;
-
-			}
-
-		}
-
+		do {
+			exibirMenuDeOpcoes();
+			int opcao = getScannerInputInt();
+			String mensagem = switchCase(opcao);
+			exibirMensagem(mensagem);
+		} while (true);
 	}
+
+	private static void exibirMenuDeOpcoes() {
+		String[] operacoes = { "Escolha uma opção: ", "(1)Create", "(2)Read", //
+				"(3)Update", "(4)Delete", "(5)Exit" };
+		for (String operacao : operacoes) {
+			System.out.println(operacao);
+		}
+	}
+
+	private static void exibirMensagem(String mensagem) {
+		System.out.print(mensagem);
+	}
+
+	private static String switchCase(int opcao) {
+		String operacao = null;
+		switch (opcao) {
+		case 1:
+			operacao = "Cadastrando...\n\n";
+			break;
+		case 2:
+			operacao = "Visualizando...\n\n";
+			break;
+		case 3:
+			operacao = "Atualizando...\n\n";
+			break;
+		case 4:
+			operacao = "Removendo...\n\n\n";
+			break;
+		case 5:
+			finalizar();
+			break;
+		}
+		return operacao;
+	}
+
+	private static void finalizar() {
+		System.out.println("finalizado.");
+		System.exit(1);
+	}
+
+	private static int getScannerInputInt() {
+		int valorInt = scanner.nextInt();
+		return valorInt;
+	}
+
 }
