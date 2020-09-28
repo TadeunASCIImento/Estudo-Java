d78383daf1cfc14a356914e4bdab48e6fee019c9
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-09-28 10:49:55 -0300

    Operações de cálculo com arrays(melhorando o código).

--- Arrays/TesteArrays04.java
+++ Arrays/TesteArrays04.java
@@ -1,38 +1,100 @@
 package br.com.estudo;
 
+import java.security.SecureRandom;
+/*
+ *  Operações de cálculo com arrays.
+ */
 public class TesteArrays04 {
+
+	private static int size = 10;// define o tamanho dos arrays.
 
 	public static void main(String[] args) {
 
-		// Declara 3 arrays de numeros inteiros.
+		// cria e exiibe 2 arrays de numeros inteiros aleatórios.
+		print("Elementos no array1: ");
+		int array1[] = gerarArrayDeInteiros(size);
+		exibirArrayDeInteiros(array1);
 
-		int x[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-		int y[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-		int z[] = new int[10];
+		print("Elementos no array2: ");
+		int array2[] = gerarArrayDeInteiros(size);
+		exibirArrayDeInteiros(array2);
 
-		System.out.print("\nSomente números pares em z: ");
+		// multiplica todos os elementos do array1 com os do array2.
+		print("Array2 multiplicado pelo Array1: ");
+		int array3[] = multiplicarArraysDeInteiros(array1, array2);
+		exibirArrayDeInteiros(array3);
 
-		// Percorre os arrays x e y.
-		for (int i = 0; i < x.length; i++) {
-			for (int j = 0; j < y.length; j++) {
+		// exibi somente os elementos pares em array1 e array2.
+		print("Somente elementos pares do array1: ");
+		exibirElementosParesDoArray(array1);
 
-				// evita erro de divisão por zero
-				if (x[i] == 0 & y[i] == 0) {// se elementos em x e y forem zero,
-					continue; // retorna para o laço for inicial.
+		print("Somente elementos pares do array2: ");
+		exibirElementosParesDoArray(array2);
 
-				} else {
-					// se não for zero multiplica os elementos e armazena em z.
-					z[i] = x[i] * y[i];
-				}
+	}
+
+	private static int[] multiplicarArraysDeInteiros(int[] array1, int[] array2) {
+		int array3[] = new int[size];
+		for (int i = 0; i < array1.length; i++) {
+			for (int j = 0; j < array2.length; j++) {
+				array3 = multiplicar(array1, array2);
 			}
-
-			if (z[i] % 2 == 0)// verifica se o elemento em z é par.
-				System.out.printf("(%d)", z[i]);// mostra o elemento se for par.
-
-			else
-				System.out.print(" ");// insere um espaço entre os números em z.
-
 		}
+		return array3;
+	}
+
+	private static int[] multiplicar(int[] array1, int[] array2) {
+		int array3[] = new int[size];
+		for (int i = 0; i < array1.length; i++) {
+			for (int j = 0; j < array2.length; j++) {
+				array3[i] = array1[i] * array2[i];
+			}
+		}
+		return array3;
+	}
+
+	private static boolean evitarDivisaoPorZero(int[] array1) {
+		for (int i = 0; i < array1.length; i++) {
+			if (array1[i] == 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static void exibirElementosParesDoArray(int[] arrayInt) {
+		for (int i = 0; i < arrayInt.length; i++) {
+			if (evitarDivisaoPorZero(arrayInt)) {
+				continue;
+			} else if (arrayInt[i] % 2 == 0) {
+				printf(arrayInt[i]);
+			}
+		}
+		print("\n");
+	}
+
+	private static void exibirArrayDeInteiros(int[] array) {
+		for (int e : array) {
+			System.out.printf(" %d ", e);
+		}
+		print("\n");
+	}
+
+	private static int[] gerarArrayDeInteiros(int size) {
+		SecureRandom random = new SecureRandom();
+		int[] arrayInt = new int[size];
+		for (int i = 0; i < arrayInt.length; i++) {
+			arrayInt[i] = (1 + random.nextInt(20));
+		}
+		return arrayInt;
+	}
+
+	private static void print(String string) {
+		System.out.print(string);
+	}
+
+	private static void printf(int inteiro) {
+		System.out.printf(" %d ", inteiro);
 	}
 
 }
