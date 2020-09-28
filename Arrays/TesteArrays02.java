0801233bae87b0fc93f135ccfa5c63c0408806c9
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-09-28 09:18:49 -0300

    Calculando a média dos elementos em um array,(melhorando o código).

--- Arrays/TesteArrays02.java
+++ Arrays/TesteArrays02.java
@@ -2,26 +2,50 @@ package br.com.estudo;
 
 import java.util.Scanner;
 
+/*
+ * Calculando a média dos elementos em um array.
+ */
+
 public class TesteArrays02 {
 
+	private static Scanner scanner = new Scanner(System.in);
+	private static String inputMessage = "informe 4 numeros?";
+	private static final int ARRAY_LENGTH = 4;
+
 	public static void main(String[] args) {
+		showInputMessage(inputMessage);
+		double[] arrayDouble = gerarArrayDouble(ARRAY_LENGTH);
+		exibirArrayDouble(arrayDouble);
+		exibirMediaDosValoresNoArrayDouble(arrayDouble);
+	}
 
-		Scanner input = new Scanner(System.in);// cria fluxo de entrada de caracteres.
-		float numeros[] = new float[3];
-		float mediaNumeros = 0;
-
-		System.out.println("\nDigite 3 números para cálculo da média.");
-		for (int cont = 0; cont < numeros.length; cont++) {
-			numeros[cont] = input.nextFloat();// solicita a entrada dos numeros via teclado.
+	private static void exibirMediaDosValoresNoArrayDouble(double[] arrayDouble) {
+		double media = 0, total = 0;
+		for (int i = 0; i < arrayDouble.length; i++) {
+			total += arrayDouble[i];
 		}
-		// Percorre o array e efetua a soma de todos elementos contidos.
-		for (int cont1 = 0; cont1 < numeros.length; cont1++) {
-			mediaNumeros += numeros[cont1];
-		}
-		// mostra no console o resultado da média dos números.
-		System.out.println("A média dos números é: " + mediaNumeros / numeros.length);
-		input.close();// Fecha o fluxo de entrada de caracteres.
+		media = total / arrayDouble.length;
+		System.out.printf("\nmédia dos elementos no array double: %.2f", media);
 
+	}
+
+	private static double[] gerarArrayDouble(int arrayLength) {
+		double[] arrayDouble = new double[arrayLength];
+		for (int i = 0; i < arrayDouble.length; i++) {
+			arrayDouble[i] = scanner.nextDouble();
+		}
+		return arrayDouble;
+	}
+
+	private static void exibirArrayDouble(double[] arrayDouble) {
+		System.out.print("elementos do array double: ");
+		for (double e : arrayDouble) {
+			System.out.printf("( %.2f )", e);
+		}
+	}
+
+	private static void showInputMessage(String message) {
+		System.out.println(message);
 	}
 
 }
