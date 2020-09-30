ba36a2445eb6e9075ea69b70eb1fdd3d4f4596aa
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-09-30 07:47:21 -0300

    Criando um ArrayList de strings e exibindo as Strings(melhorando o código).

--- ArrayList/TesteArrayList03.java
+++ ArrayList/TesteArrayList03.java
@@ -3,34 +3,47 @@ package br.com.estudo;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/*
+ *  Criando um ArrayList de strings e exibindo as Strings.
+ */
+
 public class TesteArrayList03 {
 
-	public static void main(String args[]) {
+	private static Scanner scanner = new Scanner(System.in);
 
-		Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
 
-		ArrayList<String> listaNomes = new ArrayList<String>();
+		ArrayList<String> arrayListNomes = new ArrayList<String>();
+		print("informe os nomes ou sair para finalizar:");
+		arrayListNomes = obterArraListDeNomes();
+		print("nomes no ArrayList");
+		exibirOarrayListDeNomes(arrayListNomes);
 
-		do {
-			System.out.println("Digite os nome: < Digite S para Sair >");
-			listaNomes.add(scanner.nextLine());
-
-		} while (!listaNomes.contains("S"));
-		mostrarNomes(listaNomes);
-		scanner.close();
 	}
 
-	public static void mostrarNomes(ArrayList<String> nomes) {
-
-		System.out.println("Nomes na lista:");
-		for (String str : nomes) {
-			if (str.contains("S")) {
-				System.out.println();
-			} else {
-				String strNomes = str;
-				System.out.println(strNomes);
+	// exibe os nomes em arraylistNomes.
+	private static void exibirOarrayListDeNomes(ArrayList<String> nomes) {
+		for (String nome : nomes) {
+			if (!nome.equalsIgnoreCase("Sair")) {
+				print(nome);
 			}
 		}
+
+	}
+
+	// imprime no console.
+	private static void print(Object object) {
+		System.out.println(object);
+	}
+
+	// obtem os nomes para o arrayListNomes.
+	private static ArrayList<String> obterArraListDeNomes() {
+		ArrayList<String> arrayListNomes = new ArrayList<String>();
+		// obtem entrada de nomes enquanto a entrada for diferente de sair.
+		do {
+			arrayListNomes.add(scanner.nextLine());
+		} while (!arrayListNomes.contains("Sair"));
+		return arrayListNomes;
 	}
 
 }
