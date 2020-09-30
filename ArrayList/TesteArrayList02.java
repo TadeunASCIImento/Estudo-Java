797cca3eeb965332ccd1e6464218e3880f1ce1e9
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-09-30 07:43:26 -0300

    Inserindo e removendo objetos em um ArrayList(melhorando o código).

--- ArrayList/TesteArrayList02.java
+++ ArrayList/TesteArrayList02.java
@@ -3,41 +3,98 @@ package br.com.estudo;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/*
+ *  Inserindo e removendo objetos em um ArrayList.
+ */
+
 public class TesteArrayList02 {
 
+	private static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
-
-		Scanner scanner = new Scanner(System.in);
-
-		ArrayList<String> itens = new ArrayList<String>();
-
-		do {
-			System.out.println("Digite os itens da lista ou Sair.");
-			itens.add(scanner.nextLine());
-		} while (!itens.contains("Sair"));
-
-		System.out.println("Lista de itens em Arraylist.");
-		for (int i = 0; i < itens.size(); i++) {
-			if (itens.get(i).equals("Sair"))
-				System.out.printf("");
-			else
-				System.out.println(itens.get(i));
+		ArrayList<Item> itens = new ArrayList<Item>();
+		for (int i = 0; i < 2; i++) {
+			itens.add(getItem());
 		}
+		print("\nitens no ArrayList antes da exclusão.");
+		exibirItensNoArrayList(itens);
+		itens.remove(0);// remove o item no indíce 0.
+		print("\nitens no ArrayList após a exclusão.");
+		exibirItensNoArrayList(itens);
+	}
 
+	// inclui novo item no ArrayList.
+	private static Item getItem() {
+		print("código do item?");
+		Long codigo = scanner.nextLong();
+		print("descrição do item?");
+		String descricao = scanner.next();
+		print("preço do item?\n");
+		Double preco = scanner.nextDouble();
+		Item item = new Item(codigo, descricao, preco);
+		return item;
+	}
 
-		System.out.println("Digite o nome do item para exclusão e pressione Enter.");
-		itens.remove(scanner.nextLine());
-
-		System.out.println("Lista de items em Arraylist após a exclusão.");
-		for (int i = 0; i < itens.size(); i++) {
-			if (itens.get(i).equals("Sair"))
-				System.out.printf("");
-			else
-				System.out.println(itens.get(i));
-
-			scanner.close();
+	// exibe os itens do ArrayList.
+	private static void exibirItensNoArrayList(ArrayList<Item> itens) {
+		for (Item e : itens) {
+			print(e);
 		}
+	}
 
+	// exibi as mensagens
+	private static void print(Object object) {
+		System.out.print(object);
+	}
+}
+
+class Item {
+
+	private Long codigo;
+	private String descricao;
+	private Double preco;
+
+	public Item(Long codigo, String descricao, double preco) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("\ncódigo do produto: ");
+		builder.append(codigo);
+		builder.append("\ndescrição: ");
+		builder.append(descricao);
+		builder.append("\npreço: ");
+		builder.append(preco);
+		builder.append("\n");
+		return builder.toString();
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
 	}
 
 }
