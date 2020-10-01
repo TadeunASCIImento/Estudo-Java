b81f09a54d54187f04bbd2777076f917ab1b9af6
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-10-01 09:02:17 -0300

    Analisando arquivo/diretório(melhorando o código).

--- Arquivos/AnalisaCaminhoDoArquivoOuDiretorio.java
+++ Arquivos/AnalisaCaminhoDoArquivoOuDiretorio.java
@@ -3,35 +3,68 @@ package br.com.arquivos.fluxos;
 import java.io.File;
 import java.util.Scanner;
 
-public class AnalizaCaminhoArquivo {
+/*
+ *  Analizando o caminho de um arquivo/diretório.
+ */
+
+public class AnalisaCaminhoDoArquivoOuDiretorio {
+
+	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Digite o nome do arquivo ou diretório: ");
-		analizarCaminho(scanner.nextLine());
-		scanner.close();
+		print("Informe o nome do arquivo ou diretório?");
+		String arquivo = obterNomeDoArquivoOuDiretorio();
+		analizarCaminhoDoArquivo(arquivo);
+
 	}
 
-	public static void analizarCaminho(String caminho) {
-		File arquivo = new File(caminho);
-		if (arquivo.exists()) {
-			System.out.printf("\n%s", arquivo.getName() + " existe");
-			System.out.printf("\n%s", (arquivo.isFile() ? "É um arquivo" : "não é um arquivo"));
-			System.out.printf("\n%s", (arquivo.isDirectory() ? "É diretório" : "Não é diretótio"));
-			System.out.printf("\n%s", (arquivo.isAbsolute() ? "É caminho absoluto" : "não é caminho absoluto"));
-			System.out.printf("\nUltima modificação %s ", arquivo.lastModified());
-			System.out.printf("\nTamanho %s:  ", arquivo.length());
-			System.out.printf("\nCaminho: %s ", arquivo.getPath());
-			System.out.printf("\nCaminho absoluto: %s ", arquivo.getAbsolutePath());
-			System.out.printf("\nParente: %s ", arquivo.getParent());
-			if (arquivo.isDirectory()) {
-				String[] directory = arquivo.list();
-				System.out.println("\n\nDiretório contém:\n");
-				for (String directoryName : directory)
-					System.out.println(directoryName);
+	// obtem o nome do arquivo ou diretório.
+	private static String obterNomeDoArquivoOuDiretorio() {
+		String arquivo = scanner.nextLine();
+		return arquivo;
+	}
+
+	// analiza o arquivo ou diretório.
+	private static void analizarCaminhoDoArquivo(String arquivo) {
+		File file = new File(arquivo);
+		if (file.exists()) {
+			printInfo(file);
+			if (file.isDirectory()) {
+				String[] directory = file.list();
+				print("\nLista de arquivos do diretório:\n");
+				exibirConteudoDoDiretorio(directory);
 			}
 		} else {
-			System.out.printf("%s %s", caminho, "não existe.");
+			print(arquivo.concat(" não encontrado."));
+		}
+
+	}
+
+	// exibe o conteúdo do diretório.
+	private static void exibirConteudoDoDiretorio(String[] directory) {
+		for (String name : directory) {
+			print(name);
+			print("\n");
 		}
 	}
+
+	// exibe as mensagens.
+	private static void print(Object object) {
+		System.out.print(object);
+
+	}
+
+	// exibe as informações do arquivo.
+	private static void printInfo(File file) {
+		System.out.printf("\n%s: ", file.getName().concat(" encontrado"));
+		System.out.printf("\n%s", (file.isFile() ? "é arquivo" : "não é arquivo"));
+		System.out.printf("\n%s", (file.isDirectory() ? "é diretório" : "não é diretório"));
+		System.out.printf("\nmodificado em: %s", file.lastModified());
+		System.out.printf("\ntamanho: %s", file.length());
+		System.out.printf("\nCaminho: %s", file.getPath());
+		System.out.printf("\nCaminho absoluto: %s", file.getAbsolutePath());
+		System.out.printf("\nParent: %s\n", file.getParent());
+
+	}
+
 }
