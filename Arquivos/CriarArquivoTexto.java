603af411588d0baf7db9932f1942a749585377a5
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-10-01 09:06:46 -0300

    Cria um arquivo de texto em disco(melhorando o código).

--- Arquivos/CriarArquivoTexto.java
+++ Arquivos/CriarArquivoTexto.java
@@ -1,72 +1,91 @@
 package br.com.estudo;
 
 import java.io.FileNotFoundException;
-import java.lang.SecurityException;
 import java.util.Formatter;
 import java.util.FormatterClosedException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-public class CriarArquivoTexto {
+public class CriaArquivoTexto {
 
 	private Formatter formatter;
+
 	private Scanner scanner = new Scanner(System.in);
 
+	public static void main(String[] args) {
+		CriaArquivoTexto application = new CriaArquivoTexto();
+		application.abrirArquivo();
+		application.gravar();
+		application.fecharArquivo();
+	}
+
+	// abri/cria o arquivo.
 	public void abrirArquivo() {
-
 		try {
-			// cria o arquivo no disco se houver permissão
-			formatter = new Formatter("C:Contatos.txt");
+			// cria o arquivo no disco.
+			formatter = new Formatter("c:contatos.txt");
 
-		} catch (SecurityException securityException) {
-			System.err.println("Você não tem acesso de gravação neste arquivo.");
+			// sem permissão de acesso ao disco.
+		} catch (SecurityException securiException) {
+			System.err.println("sem acesso para gravação.");
 			System.exit(1);
 
-		} catch (FileNotFoundException fileNotFoundException) {
-			System.err.println("Erro ao abrir ou criar o arquivo.");
+			// arquivo não localizado.
+		} catch (FileNotFoundException foundException) {
+			System.err.println("falha ao abrir/criar arquivo.");
 			System.exit(1);
 		}
 	}
 
-	public void inserirContato() { // metodo para escrever no arquivo.
+	// escreve no aquivo.
+	public void gravar() {
 
 		Contato contato = new Contato();
-
-		System.out.println("Entrada de dados de contato");
-		System.out.printf("%s\n%s\n%s\n%s\n\n", "Para encerrar a entrada de dados,digite:",
-				"quando você for solicitado a inserir dados.", "( No Linux / Mac  digite <ctrl> d e pressione Enter",
-				"No Windows, digite <ctrl> z e pressione Enter");
-
-		System.out.println("Digite o primeiro nome, sobrenome,telefone e email ? ");
-
-		while (scanner.hasNext()) // Loop para indicar o fim do arquivo.
-		{
+		exibirInformacoesDeEntradaDeDados();
+		while (scanner.hasNext()) {
 			try {
-
-				contato.setNome(scanner.next());
-				contato.setSobrenome(scanner.next());
-				contato.setTelefone(scanner.next());
-				contato.setEmail(scanner.next());
-
-				formatter.format("%s %s %s %s\n", contato.getNome(), contato.getSobrenome(), contato.getTelefone(),
+				contato = obterContato();
+				formatter.format("%s  %s %s %s\n\n", contato.getNome(), contato.getSobrenome(), contato.getTelefone(),
 						contato.getEmail());
 
 			} catch (FormatterClosedException formatterClosedException) {
 				System.err.println("Erro ao gravar no arquivo.");
 				return;
-			} catch (NoSuchElementException elementException) {
-				System.err.println("Dados inválidos. Por favor, tente novamente.");
+
+			} catch (NoSuchElementException noSuchElementException) {
+				System.err.println("Dados inválidos, tente novamente.");
 				scanner.nextLine();
 
 			}
-
-			System.out.println("Digite o primeiro nome, sobrenome,telefone e email ? ");
+			exibirInformacoesDeEntradaDeDados();
 		}
-		scanner.close();
 	}
 
-	public void fecharArquivo() {
-		if (formatter != null)
-			formatter.close();
+	// exibe as informações de entrada de dados de contato.
+	private void exibirInformacoesDeEntradaDeDados() {
+		System.out.println("Entrada de dados de contato:");
+		System.out.printf("%s\n%s\n%s\n%s\n\n", "Para encerrar a entrada de dados,digite:",
+				"quando você for solicitado a inserir dados.", "( No Linux / Mac  digite <ctrl> d e pressione Enter",
+				"No Windows, digite <ctrl> z e pressione Enter");
+		System.out.println("Informe o nome, sobrenome, telefone e email:");
+
 	}
+
+	// obtem os dados de contato.
+	private Contato obterContato() {
+		Contato contato = new Contato();
+		contato.setNome(scanner.next());
+		contato.setSobrenome(scanner.next());
+		contato.setTelefone(scanner.next());
+		contato.setEmail(scanner.next());
+		return contato;
+	}
+
+	// fecha o arquivo.
+	public void fecharArquivo() {
+		if (formatter != null) {
+			formatter.close();
+		}
+	}
+
 }
