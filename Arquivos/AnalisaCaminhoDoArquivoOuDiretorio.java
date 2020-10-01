ecab56f7811dca39bfc516ecf77fd6180b44a95e
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-10-01 09:01:12 -0300

    Analisando arquivo/diretorio(melhorando o código).

--- Arquivos/AnalisaCaminhoDoArquivoOuDiretorio.java
+++ Arquivos/AnalisaCaminhoDoArquivoOuDiretorio.java
@@ -6,20 +6,15 @@ import java.util.Scanner;
 public class AnalizaCaminhoArquivo {
 
 	public static void main(String[] args) {
-
 		Scanner scanner = new Scanner(System.in);
-
 		System.out.println("Digite o nome do arquivo ou diretório: ");
 		analizarCaminho(scanner.nextLine());
 		scanner.close();
 	}
 
 	public static void analizarCaminho(String caminho) {
-
 		File arquivo = new File(caminho);
-
 		if (arquivo.exists()) {
-
 			System.out.printf("\n%s", arquivo.getName() + " existe");
 			System.out.printf("\n%s", (arquivo.isFile() ? "É um arquivo" : "não é um arquivo"));
 			System.out.printf("\n%s", (arquivo.isDirectory() ? "É diretório" : "Não é diretótio"));
@@ -29,11 +24,9 @@ public class AnalizaCaminhoArquivo {
 			System.out.printf("\nCaminho: %s ", arquivo.getPath());
 			System.out.printf("\nCaminho absoluto: %s ", arquivo.getAbsolutePath());
 			System.out.printf("\nParente: %s ", arquivo.getParent());
-
 			if (arquivo.isDirectory()) {
 				String[] directory = arquivo.list();
 				System.out.println("\n\nDiretório contém:\n");
-
 				for (String directoryName : directory)
 					System.out.println(directoryName);
 			}
