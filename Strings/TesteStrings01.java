64f64e4ac8fff67b84f6cd39bb4a5bb14a4ca379
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-10-02 08:54:33 -0300

    Testando métodos da classe String e Character(melhorando código).

--- Strings/TesteStrings01.java
+++ Strings/TesteStrings01.java
@@ -1,44 +1,52 @@
-package br.com.estudo;
 
-/* Comparando Strings com os métodos equals e compareTo. */
+import java.util.Scanner;
 
+/*
+ * Testando métodos da classe String e Character.
+ */
 
 public class TesteStrings01 {
 
+	private static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
-
-		String str1 = "Manipulando Strings com os metodos da classe String";
-		String str2 = new String(str1);// constrói uma nova String com o mesmo argumento de str1.
-		String str3 = "Utilizando alguns métodos da classe String";
-		int result;
-
-		// Mostra o tamanho da String.
-		System.out.println("Tamanho de str1: " + str1.length());
-
-		for (int i = 0; i < str1.length(); i++)
-		System.out.println(str1.charAt(i));// Mostra no console cada caractere da String.
-
-		if (str1.equals(str2))// Compara str1 e str2 retorna true se for verdadeiro.
-		System.out.println("str1 é igual á str2");
-
-		else// retorna false senão forem iguais.
-		System.out.println("str1 diferente de str2");
-
-		if (str1.equals(str3))
-		System.out.println("str1 é igual á str3");
-		else
-		System.out.println("str1 diferent de str3");
-
-		result = str1.compareTo(str3);/* Compara as strings str1 e str3 pelo tamanho */
-		if (result == 0)// se forem iguais retorna 0.
-		System.out.println(" str1 e str3 são iguais");
-
-		else if (result < 0) //retorna um inteiro negativo se str1 for menor.
-		System.out.println("str1 é menor que str3");
-
-		else if (result > 0)// retorna um inteiro maior que 0 se str1 for maior
-		System.out.println("str1 é maior que str3");
-
+		print("informe string?");
+		String string = inputString();
+		char c = string.charAt(0);
+		analisarCaracteres(c);
 	}
 
+	// entrada de dados String.
+	public static String inputString() {
+		return scanner.next();
+	}
+
+	// imprime no console.
+	public static void print(String string) {
+		System.out.print(string);
+	}
+
+	// analisando o caractere.
+	private static void analisarCaracteres(char c) {
+		// verifica se é definido.
+		if (Character.isDefined(c)) {
+			print("é definido.\n");
+		}
+		// verifica se é um digito.
+		if (Character.isDigit(c)) {
+			print("é digito.");
+		}
+		// verifica se é uma letra.
+		if (Character.isLetter(c)) {
+			print("é letra");
+		}
+		// verifica se é minúscula.
+		if ((Character.isLowerCase(c))) {
+			print(" minúscula");
+		}
+		// verifica se é maiúscula.
+		if (Character.isUpperCase(c)) {
+			print(" maiúscula.");
+		}
+	}
 }
