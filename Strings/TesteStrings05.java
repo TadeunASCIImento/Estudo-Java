583fb77cd3ef0412ad05a9cc8d0244f9e36eaa08
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-10-03 07:43:19 -0300

    Criando tokens de uma frase ou texto.(melhorando o código).

--- Strings/TesteStrings05.java
+++ Strings/TesteStrings05.java
@@ -1,18 +1,39 @@
 package br.com.estudo;
 
+import java.util.Scanner;
+
+/*
+ * Criando tokens de uma frase ou texto.
+ */
+
 public class TesteStrings05 {
 
+	private static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
-
-		String strFrase = "Tokenizando uma frase ou texto com o método split";
-
-		String[] tokens = strFrase.split(" ");
-		System.out.printf("\t\tQuantidade de palavras: %d\n\n\t\tAs palavras são:\n", tokens.length);
-
-		for (String palavra : tokens)
-			System.out.println("\t\t<" + palavra.toString() + ">");
-
-		System.out.println();
-
+		String texto = inputScannerString();
+		String[] tokens = obterTokens(texto);
+		printTokens(tokens);
 	}
+
+	// exibe o texto tokenizado e a quantidade de palavras.
+	private static void printTokens(String[] tokens) {
+		System.out.printf("\nQuantidade de palavras: %d\nPalavras:\n", tokens.length);
+		for (String token : tokens) {
+			System.out.println(token.toString());
+		}
+	}
+
+	private static String inputScannerString() {
+		System.out.println("Entre com o texo?");
+		String texto = scanner.nextLine();
+		return texto;
+	}
+
+	// tokeniza o texto informado.
+	public static String[] obterTokens(String texto) {
+		texto = texto.replace(".", " ").replace(",", "");
+		return texto.split(" ");
+	}
+
 }
