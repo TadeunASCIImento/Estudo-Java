4397c0d408f13112913251e80daf74e3e07ca181
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-10-03 07:40:24 -0300

    Concatenando Strings(melhorando o código).

--- Strings/TesteStrings03.java
+++ Strings/TesteStrings03.java
@@ -1,5 +1,8 @@
 package br.com.estudo;
 
+/*
+ * 	Concatenando strings.
+ */
 public class TesteStrings03 {
 
 	public static void main(String[] args) {
@@ -8,16 +11,22 @@ public class TesteStrings03 {
 		String nome = "Tadeu";
 		String sobrenome = "Nascimento";
 
-		/* Strings antes da chamada ao método concat. */
+		/* Strings antes da chamada ao método concatenarStrings. */
 		System.out.printf("string nome = %s\nstring sobrenome = %s\n\n", nome, sobrenome);
 
 		/*
-		 * concatenando as strings nome e sobrenome chamando o metodo concat gerando uma
-		 * nova string nomeCompleto.
+		 * concatenando as strings nome e sobrenome chamando o metodo concatenarStrings
+		 * gerando uma nova string nomeCompleto.
 		 */
-		String nomeCompleto = nome.concat(" " + sobrenome);
+		String nomeCompleto = concatenarStrings(nome, sobrenome);
 
 		/* exibe a nova string nomeCompleto */
 		System.out.printf("Resultado da concatenção das strings é %s", nomeCompleto);
 	}
+
+	/* concatena e retorna a string concatenada. */
+	public static String concatenarStrings(String string1, String string2) {
+		return string1.concat(string2);
+	}
+
 }
