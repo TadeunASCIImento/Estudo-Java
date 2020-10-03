c7efaada1a48584848d302b098c2cdbc8dbe8b71
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-10-03 07:41:11 -0300

    Obtendo uma substring de uma string maior(melhornado o código).

--- Strings/TesteStrings04.java
+++ Strings/TesteStrings04.java
@@ -1,14 +1,29 @@
 package br.com.estudo;
 
+/*
+ * Obtendo uma substring de uma string maior.
+ */
+
 public class TesteStrings04 {
 
 	public static void main(String[] args) {
 
 		String str = "Usando método substring";
 
-		System.out.printf("Substring apartir do indice 10.  \"%s\"\n", str.substring(10));
+		System.out.printf("Substring a partir do indice 10.  \"%s\"\n", obterSubstring(str, 10));
 
 		System.out.printf("%s \"%s\"\n", "Substring do indice 4 até o final da String.",
-				str.substring(4, str.length()));
+				obterSubstring(str, 4, str.length()));
 	}
+
+	// retorna uma substring definindo o inicio e o fim da nova string.
+	public static String obterSubstring(String string, int inicio, int fim) {
+		return string.substring(inicio, fim);
+	}
+
+	// método sobrecarregado retorna a substring definindo o inicio.
+	public static String obterSubstring(String string, int inicio) {
+		return string.substring(inicio);
+	}
+
 }
