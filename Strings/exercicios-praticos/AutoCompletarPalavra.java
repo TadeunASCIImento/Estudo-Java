54f57e16aaa602d488f4197fa97588ad15eaddef
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-10-09 00:40:37 -0300

    Simula auto completar palavras(melhorando o código).

--- Strings/exercicios-praticos/AutoCompletarPalavra.java
+++ Strings/exercicios-praticos/AutoCompletarPalavra.java
@@ -1,32 +1,40 @@
 package br.com.estudo;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
+/*
+ * Simula auto completar palavras.
+ */
 
 public class AutoCompletarPalavra {
 
-	static Scanner scanner = new Scanner(System.in);
+	private static Scanner scanner = new Scanner(System.in);
 
-	static String strBusca;
+	private static String prefixo;// inicio da palavra a ser completada.
 
-	static String[] palavras = { "carroceiro", "carroceria", "carrocinha", "passarinho", "passageiro", "passatempo",
-			"computação", "computador", "combinação" };
+	private static List<String> palavras = Arrays.asList("carroceiro", "carroceria", "carrocinha", "carro",
+			"carrapato","passaro","passageiro","passagem");
 
 	public static void main(String[] args) {
 
-		// digite apenas uma parte da palavra contida no array
-		// e pressione enter
-		System.out.println("Digite a palavra?");
-		strBusca = scanner.next();
+		// apenas uma parte de qualquer palavra na lista palavras.
+		System.out.println("Entre com o prefixo da palavra?");
+		prefixo = scanner.next();
 
-		// exibe as palavras começadas com as letras digitadas
-		autoCompletar(strBusca);
+		// busca na lista e exibe as plavras com o prefixo informado.
+		autoCompletarPalavra(prefixo);
 
 	}
 
-	private static void autoCompletar(String strBusca) {
-		for (String str : palavras)
-			if (str.startsWith(strBusca))
-				System.out.println(str);
+	// verifica o prefixo da palavra usando startWith.
+	private static void autoCompletarPalavra(String prefixo) {
+		for (String palavra : palavras) {
+			if (palavra.startsWith(prefixo)) {
+				System.out.println(palavra);
+			}
+		}
 
 	}
 
