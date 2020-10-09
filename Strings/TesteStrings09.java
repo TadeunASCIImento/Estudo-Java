f8abe09ded257fa9e6bf197d4d08b1b24276ba21
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-10-09 00:20:16 -0300

    Metodos toLowerCase, toUpperCase ,trim e toCharArray(melhorando o código).

--- Strings/TesteStrings09.java
+++ Strings/TesteStrings09.java
@@ -1,41 +1,39 @@
 package br.com.estudo;
 
+import java.util.Scanner;
+
+/*
+ * Metodos toLowerCase, toUpperCase e trim.
+ */
+
 public class TesteStrings09 {
+
+	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
-		String string01 = "Outros métodos da classe String";
-		String string02 = "Convertendo uma String em minusculas com toLowerCase";
-		String string03 = "		espaços 	   ";
-		String string04 = "Utilisando método replace";
+		System.out.println("Entre com a string?");
+		String string = scanner.nextLine();
 
-		System.out.printf("string01 = %s\nstring02 = %s\nstring03 = %s\nstring04 = %s\n\n", string01, string02,
-				string03, string04);
+		// toUpperCase os caracteres da em maiúsculos.
+		System.out.printf("string após toUppercase: %s\n", string.toUpperCase());
 
-		// teste metodo replace, substitui caracteres na string.
-		System.out.println(string04);
-		System.out.printf("Substituir 's' por 'z' em string04: %s\n\n", string04.replace('s', 'z'));
+		// toLowerCase converte os caracteres em minusculos.
+		System.out.printf("string após toLowerCase: %s\n", string.toLowerCase());
 
-		// teste toLowerCase e toUpperCase.
-		// converte em maiusculas.
-		System.out.printf("string01.toUpperCase() = %s\n", string01.toUpperCase());
+		// remove os caracteres de espaço no inicio e fim.
+		System.out.printf("string após trim: \"%s\"\n\n", string.trim());
 
-		// converte em minusculas.
-		System.out.printf("string02.toLowerCase() = %s\n\n", string02.toLowerCase());
-
-		// Teste metodo trim.
-		// remove os caracteres de espaço no inicio e fim
-		System.out.printf("string03 após trim = \"%s\"\n\n", string03.trim());
-
-		// Teste metodo toCharArray.
-		char[] charArray = string01.toCharArray();// converte a string em um array de caracteres.
-		System.out.println("string01 como um  array de caracteres:");
+		// converte a string em um array de caracteres.
+		char[] charArray = string.toCharArray();
+		System.out.println("string0 como um  array de caracteres:");
 		for (char character : charArray) {
 			System.out.println(character);
 
 		}
 		// caracteres nos indices 0 e 3.
-		System.out.println("\n\n" + charArray[0] + charArray[3]);
+		System.out.println("\ncaracteres nos indices 0 e 3:" + charArray[0] + charArray[3]);
 
 	}
+
 }
