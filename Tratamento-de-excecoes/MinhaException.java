9158019bc7922cc66c40b618e7856bac725a4ee6
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-11-09 15:23:29 -0300

    Classe MinhaException.

---
+++ Tratamento-de-excecoes/MinhaException.java
@@ -0,0 +1,11 @@
+package br.com.tratamento.execoes;
+
+public class MinhaException extends RuntimeException {
+	private static final long serialVersionUID = -1913138775122091241L;
+
+	public MinhaException(String msg) {
+		super(msg);
+
+	}
+
+}
